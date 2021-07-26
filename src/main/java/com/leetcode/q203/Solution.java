ba290b6b389bb83db729491fe97eefe7738becf3
katus <sunkeran0818@outlook.com>
2021-07-26 16:21:14 +0800

    修正203题目解法

--- src/main/java/com/leetcode/q203/Solution.java
+++ src/main/java/com/leetcode/q203/Solution.java
@@ -5,7 +5,6 @@ import lombok.NoArgsConstructor;
 
 /**
  * 给你一个链表的头节点 head 和一个整数 val, 请你删除链表中所有满足 Node.val == val 的节点, 并返回 新的头节点
- *
  * 链接: https://leetcode-cn.com/problems/remove-linked-list-elements/
  *
  * @author SUN Katus
@@ -13,25 +12,33 @@ import lombok.NoArgsConstructor;
  */
 public class Solution {
 
+    /**
+     * 思路: 将不需要删除的节点挪动到新的链表上
+     */
     public ListNode removeElements(ListNode head, int val) {
         ListNode dummyHead = new ListNode();
         ListNode prev = dummyHead;
-        prev.next = head;
-        while (prev.next != null) {
-            if (prev.next.val == val) {
-                prev.next = prev.next.next;
+        while (head != null) {
+            if (head.val != val) {
+                prev.next = head;
+                prev = prev.next;
             }
-            prev = prev.next;
+            head = head.next;
         }
+        prev.next = null;
         return dummyHead.next;
     }
 
+    /**
+     * 思路: 将链表视为首节点和子链表两个部分
+     */
     public ListNode removeElements2(ListNode head, int val) {
         if (head != null) {
             if (head.val == val) {
-                head = head.next;
+                head = removeElements2(head.next, val);
+            } else {
+                head.next = removeElements2(head.next, val);
             }
-            head.next = removeElements2(head.next, val);
         }
         return head;
     }
@@ -41,9 +48,5 @@ public class Solution {
     public static class ListNode {
         int val;
         ListNode next;
-
-        ListNode(int val) {
-            this.val = val;
-        }
     }
 }
