7c1f7c5a46d740604f2924621f3ad379703f6432
katus <sunkeran0818@outlook.com>
2021-11-04 16:41:42 +0800

    增加JDK动态代理的注释

--- src/main/java/com/nx/vip/proxy/normal/ProxyMain.java
+++ src/main/java/com/nx/vip/proxy/normal/ProxyMain.java
@@ -16,6 +16,10 @@ public class ProxyMain {
     void test() {
         IHello hello = new Hello();
         InvocationHandler helloHandler = new ProxyHandler(hello);
+        // 通过传入的参数动态产生一个类, 动态产生内容, 然后动态生成字节码文件
+        // 参数1: 类加载器
+        // 参数2: 需要实现的接口
+        // 参数3: 指导所有需要实现的方法如何实现, 通过反射的方式
         IHello iHello = (IHello) Proxy.newProxyInstance(hello.getClass().getClassLoader(), hello.getClass().getInterfaces(), helloHandler);
         iHello.hello();
     }
