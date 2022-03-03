0ec622a68db853e3befb1388d7b159d907edc37e
katus <sunkeran0818@outlook.com>
2022-03-03 19:54:34 +0800

    新增力扣计划题目

---
+++ src/main/java/com/leetcode/q35/Solution.java
@@ -0,0 +1,30 @@
+package com.leetcode.q35;
+
+/**
+ * https://leetcode-cn.com/problems/search-insert-position/
+ * 
+ * @author SUN Katus
+ * @version 1.0, 2022-03-03
+ */
+public class Solution {
+
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (target < nums[mid]) {
+                right = mid;
+            } else if (target > nums[mid]) {
+                left = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return left;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.searchInsert(new int[]{1,3,5,6}, 4));
+    }
+}
