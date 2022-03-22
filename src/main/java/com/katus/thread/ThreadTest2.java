0f8b9357fa71d1473fab16375fd57f254f96bdd7
katus <sunkeran0818@outlook.com>
2022-03-22 17:28:29 +0800

    更新多线程任务接口泛型

--- src/main/java/com/katus/thread/ThreadTest2.java
+++ src/main/java/com/katus/thread/ThreadTest2.java
@@ -10,10 +10,10 @@ import java.util.concurrent.FutureTask;
  * @version 1.0, 2020-08-10
  */
 // 创建Callable接口的实现类
-class NumThread implements Callable {
+class NumThread implements Callable<Integer> {
     // 重写call方法 内部为线程的操作
     @Override
-    public Object call() throws Exception {   // 优势：可以抛出异常 可以有返回值 支持泛型
+    public Integer call() throws Exception {   // 优势：可以抛出异常 可以有返回值 支持泛型
         int sum = 0;
         for (int i = 0; i <= 100; i++) {
             if (i % 2 == 0) {
@@ -30,12 +30,12 @@ public class ThreadTest2 {
         // 创建Callable实现类的对象
         NumThread numThread = new NumThread();
         // 通过Callable实现类对象构建FutureTask类的对象
-        FutureTask futureTask = new FutureTask(numThread);
+        FutureTask<Integer> futureTask = new FutureTask<>(numThread);
         // 通过FutureTask类对象构建Thread类的对象 即构建线程
         new Thread(futureTask).start();
         try {
             // 下方法的返回值即为Callable实现类中call方法的返回值
-            Object sum = futureTask.get();
+            Integer sum = futureTask.get();
             System.out.println("sum = " + sum);
         } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
