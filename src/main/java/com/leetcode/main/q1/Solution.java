ada214bf56bb1377be2107f1c0b0ecc28af4ef34
katus <sunkeran0818@outlook.com>
2022-04-17 18:13:57 +0800

    新增力扣题目

--- src/main/java/com/leetcode/main/q1/Solution.java
+++ src/main/java/com/leetcode/main/q1/Solution.java
@@ -1,7 +1,9 @@
 package com.leetcode.main.q1;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
- * https://leetcode-cn.com/problems/two-sum/
  * 两数之和
  * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出和为目标值 target 的那两个整数，并返回它们的数组下标。
  * 
@@ -9,6 +11,9 @@ package com.leetcode.main.q1;
  * @version 1.0, 2022-03-03
  */
 public class Solution {
+    /**
+     * 暴力解法
+     */
     public int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length - 1; i++) {
             for (int j = i + 1; j < nums.length; j++) {
@@ -16,6 +21,20 @@ public class Solution {
                     return new int[] { i, j };
                 }
             }
+        }
+        return new int[0];
+    }
+
+    /**
+     * 哈希表
+     */
+    public int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (hashMap.containsKey(target - nums[i])) {
+                return new int[]{hashMap.get(target - nums[i]), i};
+            }
+            hashMap.put(nums[i], i);
         }
         return new int[0];
     }
