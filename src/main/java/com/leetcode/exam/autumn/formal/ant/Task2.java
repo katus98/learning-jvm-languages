097bdd340ed75abfa51c3067f776900ca9554dd0
katus <sunkeran0818@outlook.com>
2022-09-22 10:33:51 +0800

    新增蚂蚁笔试

---
+++ src/main/java/com/leetcode/exam/autumn/formal/ant/Task2.java
@@ -0,0 +1,36 @@
+package com.leetcode.exam.autumn.formal.ant;
+
+import java.util.Scanner;
+
+/**
+ * @author SUN Katus
+ * @version 1.0, 2022-09-22
+ */
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int times = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < times; i++) {
+            String[] items = scanner.nextLine().split(" ");
+            int n = Integer.parseInt(items[0]);
+            int m = Integer.parseInt(items[1]);
+            int count = 0;
+            if (n - 1 > 0) {
+                if ((n - 1) % 2 == 1) {
+                    count += 1;
+                } else {
+                    count += 2;
+                }
+            }
+            if (m - 1 > 0) {
+                if ((m - 1) % 2 == 1) {
+                    count += 1;
+                } else {
+                    count += 2;
+                }
+            }
+            System.out.println(count);
+        }
+        scanner.close();
+    }
+}
---
+++ src/main/java/com/leetcode/exam/autumn/formal/ant/Task3.java
@@ -0,0 +1,67 @@
+package com.leetcode.exam.autumn.formal.ant;
+
+import java.util.*;
+
+/**
+ * @author SUN Katus
+ * @version 1.0, 2022-09-22
+ */
+public class Task3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        scanner.close();
+        System.out.println(process2(line));
+    }
+
+    private static long process2(String line) {
+        int wholeLength = line.length();
+        Map<Integer, Integer> map2 = new HashMap<>();
+        Map<Integer, Integer> map3 = new HashMap<>();
+        for (int i = 0; i < wholeLength; i++) {
+            int j = i + 1;
+            if (!valid(j, wholeLength)) continue;
+            if (line.charAt(i) == line.charAt(j)) {
+                map2.put(i, j);
+            }
+            j = i + 2;
+            if (!valid(j, wholeLength)) continue;
+            if (line.charAt(i) == line.charAt(j)) {
+                map3.put(i, j);
+            }
+        }
+        long count = wholeLength;
+        int i = 0;
+        while (i < wholeLength) {
+            if (map2.containsKey(i)) {
+                i += 1;
+                continue;
+            }
+            if (map3.containsKey(i)) {
+                count += 2;
+                i += 2;
+                continue;
+            }
+            int start = i;
+            while (i < wholeLength && !map2.containsKey(i) && !map3.containsKey(i)) {
+                i++;
+            }
+            if (i == wholeLength) {
+                i -= 1;
+                int c = i - start + 1;
+                int cc2 = c  * (c + 1) / 2 - c;
+                count += cc2;
+                break;
+            }
+            int c = i - start + 1;
+            int cc2 = c  * (c + 1) / 2 - c;
+            count += cc2;
+            i++;
+        }
+        return count;
+    }
+
+    private static boolean valid(int i, int wholeLength) {
+        return i >= 0 && i < wholeLength;
+    }
+}
